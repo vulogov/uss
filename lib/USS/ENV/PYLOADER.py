45ae0ffed270962605f4c9ca9800eefe7bc22cf6
vulogov <vladimir.ulogov@me.com>
2016-01-10 16:58:26 -0800

    Add support for future submitting of the facts from which modules and functions are loaded.

--- lib/USS/ENV/PYLOADER.py
+++ lib/USS/ENV/PYLOADER.py
@@ -2,11 +2,30 @@ __author__  =  'Vladimir Ulogov'
 __version__ = 'v0.1.0'
 
 import imp
+import types
 from FS import *
 
 class PYLOADER:
     def __init__(self):
         self.mods = {}
+    def module_loaded(self, mod, fun):
+        pass
+    def mod_exec(self, _mod):
+        if type(_mod) == types.StringType:
+            ## Passing the name
+            _mod = self.find_the_mod(_mod)
+            if _mod == None:
+                return []
+        elif type(_mod) == types.ModuleType:
+            _mod = _mod
+        else:
+            return []
+        out = []
+        for f in dir(_mod):
+            if type(getattr(_mod, f)) != type.FunctionType:
+                continue
+            out.append(f)
+        return out
     def find_the_mod(self, mod_name):
         for p in self.mods.keys():
             if self.mods[p].has_key(mod_name):
@@ -24,9 +43,13 @@ class PYLOADER:
                     continue
                 try:
                     _mod = imp.load_source(modname, full_path)
-                except KeyboardInterrupt:
+
+                except:
                     continue
                 self.mods[p][modname] = _mod
+                f_list = self.mod_exec(_mod)
+                for f in f_list:
+                    self.module_loaded(modname, f)
         for p in self.mods.keys():
             if p not in self.path:
                 del self.mods[p]
